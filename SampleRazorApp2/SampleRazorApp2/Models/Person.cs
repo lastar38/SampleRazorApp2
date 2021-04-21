cb2bd9d8d7dcbd5887d9ca935c9754f34540bafa
lastar38 <lastarmain@gmail.com>
2021-04-21 22:30:23 +0900

    エラーメッセージ日本語

--- SampleRazorApp2/SampleRazorApp2/Models/Person.cs
+++ SampleRazorApp2/SampleRazorApp2/Models/Person.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SampleRazorApp2.Models
 {
     public class Person
     {
         public int PersonId { get; set; }
+     
+        [Display(Name = "名前")]
+        [Required(ErrorMessage = "必須項目です")]
         public string Name { get; set; }
+        [Display(Name = "メールアドレス")]
+        [EmailAddress(ErrorMessage ="メールアドレスが必要です")]
         public string Mail { get; set; }
+        [Display(Name="年齢")]
+        [Range(0,200, ErrorMessage ="0以上200以下の値にしてください")]
         public int Age { get; set; }
     }
 }
